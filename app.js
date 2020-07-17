670a662651fd3761d075352a314313f4013933f8
Andrew Frazier <acorbettfrazier@gmail.com>
2020-07-17 13:26:22 -0400

    controller has access to other two modules

--- app.js
+++ app.js
@@ -4,8 +4,27 @@ var budgetController = (function () {
   var add = function (a) {
     return a + x;
   };
+
+  return {
+    publicTest: function (a) {
+      return add(a);
+    },
+  };
 })();
 
 var UIController = (function () {
   // add code here
 })();
+
+// THERE NEEDS TO BE A WAY TO ADD STUFF TO THE UI AND THEN PASS IT TO THE BUDGET SO YOU NEED TO HAVE THE CONTROLLER BELOW
+// WE WILL PASS THE OTHER 2 CONTROLLERS AS ARGUEMENTS SO IT KNOWS THEY EXIST AND CAN CONNECT THEM
+// now we pass them as arguements
+var controller = (function (budgetCtrl, UICtrl) {
+  z = budgetCtrl.publicTest(9);
+
+  return {
+    test2: function () {
+      console.log(z);
+    },
+  };
+})(budgetController, UIController);
