bea457978f266800ba098a0eedb56931f58e1510
Andrew Frazier <acorbettfrazier@gmail.com>
2020-07-17 16:14:23 -0400

    dom document selectors set to object holder

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-  //   some code
+  var domStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+  };
+  return {
+    getinput: function () {
+      return {
+        type: document.querySelector(domStrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(domStrings.inputDescription).value,
+        value: document.querySelector(domStrings.inputValue).value,
+      };
+    },
+  };
 })();
 
 // THERE NEEDS TO BE A WAY TO ADD STUFF TO THE UI AND THEN PASS IT TO THE BUDGET SO YOU NEED TO HAVE THE CONTROLLER BELOW
@@ -17,11 +30,12 @@ var controller = (function (budgetCtrl, UICtrl) {
   var controlAddItem = function () {
     // TO DO LIST
     // 1. GET THE FILED INPUT DATA
+    var input = UICtrl.getinput();
+    console.log(input);
     // 2. ADD THE ITEM TO THE BUDGET CONTROLLER
     // 3. ADD THE ITEM TO THE UI
     // 4. CALUCLATE THE BUDGET
     // 5. DIPLAY ON THE UI
-    console.log("testing testing ");
   };
   document.querySelector(".add__btn").addEventListener("click", controlAddItem);
   //   this is being assinged to the global document for use
